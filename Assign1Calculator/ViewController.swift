5818f72b40466a67cb28713921460711a8f18e7f
guilherme <gtmorais@gmail.com>
2017-01-15 11:29:23 -0500

    Initial Commit

---
+++ Assign1Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assign1Calculator
+//
+//  Created by Guilherme Morais on 2017-01-15.
+//  Copyright © 2017 Guilherme Morais. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
