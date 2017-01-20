350523c425be693f862f8efb2f0b126cb4f6acc7
guilherme <gtmorais@gmail.com>
2017-01-20 15:27:20 -0500

    Start coding

--- Assign1Calculator/ViewController.swift
+++ Assign1Calculator/ViewController.swift
@@ -8,8 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum operation {
+   case percent, add, minus, mult, none
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    var operationTapped:Bool = false
+    var currentValue:String = "0"
+    var currentOperation:operation = .none
+    var savedNum:Int = 0
+    
+    func showValue() {
+        guard let labelInt:Int = Int(currentValue) else {
+            return
+        }
+        if (currentOperation == .none) {
+            savedNum = labelInt
+        }
+        
+        let formatter:NumberFormatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        let num:NSNumber = NSNumber(value: labelInt)
+        mainLabel.text = formatter.string(from: num)
+    }
+
+    func changeOperation(newOperation:operation)
+    {
+        currentOperation = newOperation
+        operationTapped = true
+    }
+    
+    @IBAction func numberTap(_ sender: UIButton) {
+        let stringValue:String? = sender.titleLabel?.text
+        
+        
+        
+        
+        if (operationTapped) {
+            operationTapped = false
+            currentValue = "0"
+        }
+        
+        currentValue = currentValue.appending(stringValue!)
+        showValue()
+    }
+    
 //    @IBAction func buttonTap(_ sender: UIButton) {
 //        if (sender.value(forKey: <#T##String#>))
 //    }
@@ -23,7 +68,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
